4f00bc944cf191755db682a936d3250c823373b6
benoitlord <benoitlord98@outlook.com>
2021-03-12 16:34:49 -0500

    Created the empty component for the back to top button in preparation to adding it

---
+++ components/backToTop.vue
@@ -0,0 +1,86 @@
+<template>
+  
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+
+    methods: {
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+  /* VARIABLES */
+
+    /* COULEURS */
+
+  $vert: #68b32d;
+  $vertFonce: #5AA91E;
+  $gris: #dddddd;
+  $grisFonce: #bbbbbb;
+  $grisFonceFonce: #aaaaaa;
+  $grisHautCTA: #333333;
+  $grisHautCTAFonce: #000000;
+  $noir: #000000;
+  $blanc: #ffffff;
+
+  /* MIXINS */
+
+  @mixin transition($vitesse, $easing: ease) {
+    transition: all $vitesse $easing;
+    -webkit-transition: all $vitesse $easing;
+    -moz-transition: all $vitesse $easing;
+    -o-transition: all $vitesse $easing;
+    -ms-transition: all $vitesse $easing;
+  }
+
+  @mixin spinKeyframes($nom, $debut, $fin){
+    @keyframes #{$nom} {
+      0% { transform: rotate($debut); }
+      100% { transform: rotate($fin); }
+    }
+    @-webkit-keyframes #{$nom} {
+      0% { transform: rotate($debut); }
+      100% { transform: rotate($fin); }
+    }
+    @-moz-keyframes #{$nom} {
+      0% { transform: rotate($debut); }
+      100% { transform: rotate($fin); }
+    }
+    @-o-keyframes #{$nom} {
+      0% { transform: rotate($debut); }
+      100% { transform: rotate($fin); }
+    }
+  }
+
+  @mixin spinAnimation($nom, $duree){
+    animation: $nom $duree linear infinite;
+    -webkit-animation: $nom $duree linear infinite;
+    -moz-animation: $nom $duree linear infinite;
+    -ms-animation: $nom $duree linear infinite;
+    -o-animation: $nom $duree linear infinite;
+  }
+
+</style>
+
+<i18n>
+
+  {
+    "en":{
+      
+    },
+    "fr":{
+      
+    }
+  }
+
+</i18n>
