0f1799b4d9710e58f4b90420e5ba19074b397472
benoitlord <benoitlord98@outlook.com>
2021-03-13 09:38:17 -0500

    Fixed some things on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <langswitch />
     <b-container>
       <b-row align-h="center">
-        <b-col cols="4" sm="2" md="1">
+        <b-col cols="3" sm="2" lg="1">
           <nuxt-link :to="localePath('/')"><img @mouseover="logoHover = true" @mouseleave="logoHover = false" class="img-fluid" alt="Logo" :src="require('~/assets/logo_' + (logoHover ? 'couleur' : 'nb') + '.png')" /></nuxt-link>
         </b-col>
       </b-row>
@@ -14,7 +14,7 @@
     </b-container>
     <b-container fluid tag="footer">
       <b-row align-h="center" style="margin-bottom: 25px;">
-          <b-col cols="10" sm="6" md="4" class="social-icons">
+          <b-col cols="12" class="social-icons">
             <social name="twitter" link="https://twitter.com/tiben234"></social>
             <social name="behance" link="https://www.behance.net/benoitlord98"></social>
             <social name="linkedin" link="http://www.linkedin.com/in/benoit-lord"></social>
@@ -23,7 +23,7 @@
           </b-col>
       </b-row>
       <b-row align-h="center">
-          <b-col cols="10" sm="6" md="4">
+          <b-col cols="12">
             <p class="text-center">&copy;&nbsp;Benoit Lord, 2021</p>
           </b-col>
       </b-row>
@@ -70,7 +70,7 @@
     justify-content: center;
     margin-top: 70px;
 
-    .social:not(last-child){
+    .social:not(:last-child){
       margin-right: 10px;
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -6,28 +6,28 @@
         <h2 class="text-center" style="margin-bottom: 50px; font-size: 1.3em;">{{ $t('titre') }}</h2>
       </b-col>
     </b-row>
-    <b-row>
-      <b-col cols="12" sm="6">
-        <img class="img-fluid" src="~assets/accueil_moi_v2.jpg" :alt="(($i18n.locale === 'en') ? 'Image of ' : 'Image de ') + 'Benoit Lord'">
+    <b-row align-v="center">
+      <b-col cols="12" lg="6">
+        <img class="img-fluid" style="margin-top: 20px; margin-bottom: 20px;" src="~assets/accueil_moi_v2.jpg" :alt="(($i18n.locale === 'en') ? 'Image of ' : 'Image de ') + 'Benoit Lord'">
       </b-col>
-      <b-col cols="12" sm="6" v-if="$i18n.locale === 'en'">
+      <b-col cols="12" lg="6" v-if="$i18n.locale === 'en'">
         <h2>Qui suis-je?</h2>
         <p>Mon premier contact avec le design graphique s’est fait lorsque j’ai commencé mes études en intégration multimédia au cégep. J’ai toujours eu un certain intérêt pour le domaine, mais c’est à ce moment que j’ai été plongé dans le design et me suis vraiment intéressé à ses nombreuses possibilités en termes de communication et d’organisation d’information.</p>
         <p>Toutefois, ce programme m’a aussi permis de découvrir un autre champ que j’affectionne beaucoup : le développement web. Le web, grâce à l’infinité des possibilités permises par la technologie, est un domaine très stimulant et intéressant pour moi et, en mettant à profit mon côté logique et mathématique bien approfondis pour apprendre très rapidement, je suis rapidement tombé en amour avec la programmation et tout au long de ma formation en multimédia, je me suis nourri de cette passion. ​​Dans le cadre de mon emploi à l'École de la fonction publique du Canada, j'ai aussi eu l'occasion de beaucoup développer ce côté, particulièrement mes compétences en développement, en travail d'équipe, en accessibilité web, et en conception d'interfaces et d'expériences utilisateurs.</p>
       </b-col>
-      <b-col cols="12" sm="6" v-if="$i18n.locale === 'fr'">
+      <b-col cols="12" lg="6" v-if="$i18n.locale === 'fr'">
         <h2>Qui suis-je?</h2>
         <p>Mon premier contact avec le design graphique s’est fait lorsque j’ai commencé mes études en intégration multimédia au cégep. J’ai toujours eu un certain intérêt pour le domaine, mais c’est à ce moment que j’ai été plongé dans le design et me suis vraiment intéressé à ses nombreuses possibilités en termes de communication et d’organisation d’information.</p>
         <p>Toutefois, ce programme m’a aussi permis de découvrir un autre champ que j’affectionne beaucoup : le développement web. Le web, grâce à l’infinité des possibilités permises par la technologie, est un domaine très stimulant et intéressant pour moi et, en mettant à profit mon côté logique et mathématique bien approfondis pour apprendre très rapidement, je suis rapidement tombé en amour avec la programmation et tout au long de ma formation en multimédia, je me suis nourri de cette passion. ​​Dans le cadre de mon emploi à l'École de la fonction publique du Canada, j'ai aussi eu l'occasion de beaucoup développer ce côté, particulièrement mes compétences en développement, en travail d'équipe, en accessibilité web, et en conception d'interfaces et d'expériences utilisateurs.</p>
       </b-col>
     </b-row>
     <b-row align-h="center">
-      <b-col cols="12" sm="8" class="text-center" v-if="$i18n.locale === 'en'">
+      <b-col cols="12" md="8" class="text-center" v-if="$i18n.locale === 'en'">
         <h2>Comment est-ce que je crée mes projets?</h2>
         <p>En raison de mon penchant pour la technologie, une grande partie du travail que je fais en ce qui a trait au design se fait avec l’aide d’une multitude d’outils technologiques, le plus souvent mon ordinateur équipé de divers logiciels de prise de notes, d’idéation et de travail ainsi qu’à mon iPad et son Apple Pencil me permettant la liberté de l’écriture et du dessin à la main.</p>
         <p>Mon processus créatif, de manière générale, commence souvent par la génération d’idée. Pour ce faire, je vais habituellement faire de la recherche sur le sujet de mon projet pour ensuite créer un brainstorm ou un mindmap qui me permettra de ressortir les mots-clés, les thèmes importants et l’ambiance générale qui entoure du sujet. Par la suite, ma prochaine étape sera la création d’une série d’esquisses préliminaires afin de mettre sur papier (ou dans mon cas sur écran) les nombreuses idées qui germent dans ma tête. Parmi celles-ci je vais par la suite chercher les 1-3 qui ont le plus de potentiel afin de les pousser plus loin, en esquisses plus complètes. De ces dernières, je vais ensuite extraire le produit final.</p>
       </b-col>
-      <b-col cols="12" sm="8" class="text-center" v-if="$i18n.locale === 'fr'">
+      <b-col cols="12" md="8" class="text-center" v-if="$i18n.locale === 'fr'">
         <h2>Comment est-ce que je crée mes projets?</h2>
         <p>En raison de mon penchant pour la technologie, une grande partie du travail que je fais en ce qui a trait au design se fait avec l’aide d’une multitude d’outils technologiques, le plus souvent mon ordinateur équipé de divers logiciels de prise de notes, d’idéation et de travail ainsi qu’à mon iPad et son Apple Pencil me permettant la liberté de l’écriture et du dessin à la main.</p>
         <p>Mon processus créatif, de manière générale, commence souvent par la génération d’idée. Pour ce faire, je vais habituellement faire de la recherche sur le sujet de mon projet pour ensuite créer un brainstorm ou un mindmap qui me permettra de ressortir les mots-clés, les thèmes importants et l’ambiance générale qui entoure du sujet. Par la suite, ma prochaine étape sera la création d’une série d’esquisses préliminaires afin de mettre sur papier (ou dans mon cas sur écran) les nombreuses idées qui germent dans ma tête. Parmi celles-ci je vais par la suite chercher les 1-3 qui ont le plus de potentiel afin de les pousser plus loin, en esquisses plus complètes. De ces dernières, je vais ensuite extraire le produit final.</p>
